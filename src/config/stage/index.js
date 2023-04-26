bc2b1a870eb8ef801ab30727a164a1c8ecc888b9
qiulonghui <569374939@qq.com>
2023-04-26 15:47:23 +0800

    20230426

--- src/config/stage/index.js
+++ src/config/stage/index.js
@@ -49,64 +49,64 @@ let homeRouter = [
 ]
 
 // 接入插件
-const plugins = [...pluginsConfig]
-filterPlugin(homeRouter)
-homeRouter = homeRouter.concat(plugins)
+// const plugins = [...pluginsConfig]
+// filterPlugin(homeRouter)
+// homeRouter = homeRouter.concat(plugins)
 
 // 处理顺序
-homeRouter = Utils.sortByOrder(homeRouter)
-deepReduceName(homeRouter)
+// homeRouter = Utils.sortByOrder(homeRouter)
+// deepReduceName(homeRouter)
 
 export default homeRouter
 
 /**
  * 筛除已经被添加的插件
  */
-function filterPlugin(data) {
-  if (plugins.length === 0) {
-    return
-  }
-  if (Array.isArray(data)) {
-    data.forEach(item => {
-      filterPlugin(item)
-    })
-  } else {
-    const findResult = plugins.findIndex(item => data === item)
-    if (findResult >= 0) {
-      plugins.splice(findResult, 1)
-    }
-    if (data.children) {
-      filterPlugin(data.children)
-    }
-  }
-}
+// function filterPlugin(data) {
+//   if (plugins.length === 0) {
+//     return
+//   }
+//   if (Array.isArray(data)) {
+//     data.forEach(item => {
+//       filterPlugin(item)
+//     })
+//   } else {
+//     const findResult = plugins.findIndex(item => data === item)
+//     if (findResult >= 0) {
+//       plugins.splice(findResult, 1)
+//     }
+//     if (data.children) {
+//       filterPlugin(data.children)
+//     }
+//   }
+// }
 
 /**
  * 使用 Symbol 处理 name 字段, 保证唯一性
  */
-function deepReduceName(target) {
-  if (Array.isArray(target)) {
-    target.forEach(item => {
-      if (typeof item !== 'object') {
-        return
-      }
-      deepReduceName(item)
-    })
-    return
-  }
-  if (typeof target === 'object') {
-    if (typeof target.name !== 'symbol') {
-      target.name = target.name || Utils.getRandomStr()
-      target.name = Symbol(target.name)
-    }
+// function deepReduceName(target) {
+//   if (Array.isArray(target)) {
+//     target.forEach(item => {
+//       if (typeof item !== 'object') {
+//         return
+//       }
+//       deepReduceName(item)
+//     })
+//     return
+//   }
+//   if (typeof target === 'object') {
+//     if (typeof target.name !== 'symbol') {
+//       target.name = target.name || Utils.getRandomStr()
+//       target.name = Symbol(target.name)
+//     }
 
-    if (Array.isArray(target.children)) {
-      target.children.forEach(item => {
-        if (typeof item !== 'object') {
-          return
-        }
-        deepReduceName(item)
-      })
-    }
-  }
-}
+//     if (Array.isArray(target.children)) {
+//       target.children.forEach(item => {
+//         if (typeof item !== 'object') {
+//           return
+//         }
+//         deepReduceName(item)
+//       })
+//     }
+//   }
+// }
